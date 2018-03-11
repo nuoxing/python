614d1525566ab07c289dea9e3c230349dbba4d2f
swy <11@qq.com>
2018-03-11 21:47:58 +0800

    添加注释

--- tutorial/tutorial/csvpipeline.py
+++ tutorial/tutorial/csvpipeline.py
@@ -10,6 +10,6 @@ class CSVPipeline(object):
     list1 = [];
     list1.append(item['title'])
     print(list1)
-    self.csvwriter.writerow(list1)
+    self.csvwriter.writerow(list1)#必须传入list类型的数据  不然自动转类型过程中 输出的数据会有问题 例如传入的是字符串 百度以下 转list  变成 百，度，一，下
 
     return item
