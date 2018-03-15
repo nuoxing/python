e9c0cf5f896c72edbffc4ecd3eb2509e15282237
swy <11@qq.com>
2018-03-15 21:45:57 +0800

    修改

--- ipets-v2.0/ipets/csvpipeline.py
+++ ipets-v2.0/ipets/csvpipeline.py
@@ -4,11 +4,10 @@ import itertools
 把爬取的数据保存到cvs
 """
 class CSVPipeline(object):
-  def __init__(self):
-    self.csvwriter = csv.writer(open('E:/items.csv', 'w', encoding='gbk',newline=''), dialect='excel')
-    #self.csvwriter.writerow(['title'])
 
   def process_item(self, item, ampa):
-    self.csvwriter.writerow(item['reslist'])#必须传入list类型的数据  不然自动转类型过程中 输出的数据会有问题 例如传入的是字符串 百度以下 转list  变成 百，度，一，下
+    with open('E:/items.csv', 'w', encoding='gbk',newline='') as f:
+      csvwriter = csv.writer(f, dialect = 'excel')
+      csvwriter.writerow(item['reslist'])#必须传入list类型的数据  不然自动转类型过程中 输出的数据会有问题 例如传入的是字符串 百度以下 转list  变成 百，度，一，下
 
     return item
